01c94a54319a085efb64a78d4773a3af4369abd9
melanyJazminH009 <melany.herrera090@gmail.com>
2025-08-16 15:46:40 -0300

    Agrego eventos.js y div con alert

---
+++ eventos.js
@@ -0,0 +1,7 @@
+// Seleccionamos el div
+const div = document.getElementById('miDiv');
+
+// Agregamos un manejador de eventos para el click
+div.addEventListener('click', function() {
+  alert('Hola! Soy el div');
+});
